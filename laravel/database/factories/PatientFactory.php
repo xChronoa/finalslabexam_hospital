b28b2c8f74bfa016dcd6331ac391fb81aa13bb61
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-06-05 14:06:33 +0800

    Created Patient Factory

---
+++ laravel/database/factories/PatientFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Patient>
+ */
+class PatientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'first_name' => fake()->name(),
+            'last_name' => fake()->name(),
+            'date_of_birth' => fake()->date(),
+            'gender' => fake()->text(),
+            'address' => fake()->address(),
+            'phone' => fake()->phoneNumber(),
+            'email' => fake()->unique()->safeEmail(),
+            'emergency_contact' => fake()->phoneNumber(),
+        ];
+    }
+}
