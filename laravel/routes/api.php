d3ccdbf0db2ee633d99a61f902facf90b0dc952a
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-06-05 14:24:00 +0800

    Created API routes for Appointments

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppointmentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix("/appointments")->group(function () {
+    Route::get("/view", [AppointmentController::class, 'view']);
+    Route::get("/view/patient/{id}", [AppointmentController::class, 'patientView']);
+    Route::post("/book", [AppointmentController::class, 'book']);
+    Route::put("/reschedule/{id}", [AppointmentController::class, 'reschedule']);
+    Route::delete("/cancel/{id}", [AppointmentController::class, 'cancel']);
 });
